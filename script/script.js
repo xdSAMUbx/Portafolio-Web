e915b45a3e7d2ab114857062c479108a059abda8
Samuel Calderon <samuelcalderon1202@hotmail.com>
2024-09-04 07:39:33 -0500

    correcciones

--- script/script.js
+++ script/script.js
@@ -1,7 +1,18 @@
-function callback(){
+let lastScrollTop = 0;
+const header = document.querySelector('header');
 
-}
+window.addEventListener('scroll', function() {
+    window.requestAnimationFrame(function() {
+        let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
 
-
-
-const observer = new IntersectionObserver(callback, options);
+        if (currentScroll > lastScrollTop) {
+            // Scroll hacia abajo, ocultamos el header
+            header.style.top = '-4rem';
+        } else {
+            // Scroll hacia arriba, mostramos el header
+            header.style.top = '0rem';
+        }
+        // Actualizamos lastScrollTop evitando valores negativos
+        lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+    });
+});
